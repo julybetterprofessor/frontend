8ca3dc94bb779e04b2b15b4e27f027b6fad279c5
JasonYoo1 <jyoojs@gmail.com>
2019-07-30 12:39:36 -0700

    jason-yoo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Form } from './components/Form';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
        <Form/>
+        <Route exact path='/form' component={Form} />
     </div>
   );
 }
