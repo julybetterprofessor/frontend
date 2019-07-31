73898263f20060300013c2d760e3fd12a2f49583
dustyfingers <dustyfingers01257@gmail.com>
2019-07-30 21:59:49 -0600

    idk and I dont care any more

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Register from './components/register/Register';
-import { Form } from './components/Form';
-import { StudentApp } from "./components/Students/StudentApp"
 
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Register from './components/register/Register';
+import { StudentApp } from "./components/Students/StudentApp";
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/navbar/Navbar';
 import SignIn from './components/signin/SignIn';
@@ -19,9 +15,7 @@ function App() {
       <Navbar isAuthenticated={isAuthenticated} />
       <Switch>
         <Route exact path='/register' component={ Register } />
-        <Route exact path='/signin' component={ Form } />
         <Route exact path='/studentform' component = { StudentApp }/>
-        <Route exact path='/register' component={Register} />
         <Route exact path='/signin' render={() => <SignIn setAuth={setIsAuthenticated} />} />
         <Route exact path='/dashboard' component={Dashboard} />
       </Switch>
