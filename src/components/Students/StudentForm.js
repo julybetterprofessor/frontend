326def53adb7eb696300862834702ed97be8d703
dustyfingers <dustyfingers01257@gmail.com>
2019-07-31 16:07:23 -0600

    added routing to project, built dashboard and wired up create student from

--- src/components/Students/StudentForm.js
+++ src/components/Students/StudentForm.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 //creating form for students to input information
 //please use StudentForm to render form on page
-export const StudentForm = () => {
+export const StudentForm = ({ history }) => {
 
     const [student, setStudent] = useState({
         firstname: '',
         lastname: '',
-        email:'',
+        email: '',
     });
 
     const {
@@ -18,8 +19,8 @@ export const StudentForm = () => {
     } = student;
 
     //setting target values
-    const handleChange = event =>{
-        setStudent({...student, [event.target.name] : event.target.value});
+    const handleChange = event => {
+        setStudent({ ...student, [event.target.name]: event.target.value });
     };
 
     //created eventlistener for submit aka onsubmit
@@ -34,45 +35,43 @@ export const StudentForm = () => {
         try {
             const config = {
                 headers: {
-                  authorization: localStorage.getItem("token")
+                    authorization: localStorage.getItem("token")
                 }
             };
-            const body = JSON.stringify(newStudent);
-            console.log(body, config);
-            const res = await axios.post('https://better-professor-app-backend.herokuapp.com/api/students', body, config);
-            console.log(res);
-
+            const body = newStudent;
+            await axios.post('https://better-professor-app-backend.herokuapp.com/api/students', body, config);
+            history.push('/dashboard');
         } catch (err) {
             console.error(err.response.data);
         }
     }
 
     //creating form
-    return(
+    return (
         <div className="formBorder">
             <h1 className="large text-primary">New Student</h1>
             <p className="lead"><i className="fas fa-user"></i> Create a new student</p>
             <form onSubmit={evt => handleSubmit(evt)}>
                 <div className="form-group">
-                    <input placeholder='First Name' 
+                    <input placeholder='First Name'
                         value={student.firstname}
                         name="firstname"
-                        onChange={handleChange}/>
+                        onChange={handleChange} />
                 </div>
 
                 <div className="form-group">
-                    <input placeholder='Last Name' 
+                    <input placeholder='Last Name'
                         value={student.lastname}
                         name="lastname"
-                        onChange={handleChange}/>
+                        onChange={handleChange} />
                 </div>
 
                 <div className="form-group">
-                    <input placeholder='Email' 
+                    <input placeholder='Email'
                         value={student.email}
-                        type = "email"
+                        type="email"
                         name="email"
-                        onChange={handleChange}/>
+                        onChange={handleChange} />
                 </div>
 
                 <input type="submit" className="btn btn-primary" value="Create Student" />
@@ -82,4 +81,4 @@ export const StudentForm = () => {
 
 }
 
-export default StudentForm
+export default withRouter(StudentForm);
