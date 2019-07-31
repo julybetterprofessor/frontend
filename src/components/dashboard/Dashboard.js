3b4d8420f39aa5f1ac89b28d483f6eb173e3b8ce
dustyfingers <dustyfingers01257@gmail.com>
2019-07-31 12:55:07 -0600

    fixed api endpoint nightmare

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,15 +5,12 @@ import axios from 'axios';
 const Dashboard = () => {
     const getStudents = async () => {
         console.log('getStudents has been run!');
-        const config = JSON.stringify({
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('professor')
-            }
-        });
-        console.log(config);
-        let res = await axios.get( 'https://better-professor-app-backend.herokuapp.com/api/professor-student-info', null, config);
-        console.log(res);
+        const config = {
+          headers: {
+            authorization: localStorage.getItem("token")
+          }
+        };
+        let res = await axios.get('https://better-professor-app-backend.herokuapp.com/api/professor-student-info', config);
     };
   
     useEffect(() => {
